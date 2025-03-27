61081004101b95ab650e43a064728d8acd344f84
jchr <jicelejeune@gmail.com>
2025-03-28 00:06:36 +0100

    ajout la possibilité d'ajouter une nouvelle radio, mais manque la persistance

--- src/webradio/model.clj
+++ src/webradio/model.clj
@@ -6,7 +6,12 @@
     {:name "France Inter" :url "http://direct.franceinter.fr/live/franceinter-midfi.mp3"}
     {:name "Nova" :url "http://radionova.ice.infomaniak.ch/radionova-256.aac"}
     {:name "Shonan Beach FM" :url "http://shonanbeachfm.out.airtime.pro:8000/shonanbeachfm_a"}
-    {:name "CHOI 981 Radio X" :url "https://lb0-stream.radiox981.ca/choi.mp3"}
-    ]))
+    {:name "CHOI 981 Radio X" :url "https://lb0-stream.radiox981.ca/choi.mp3"}]))
 
 (def current-player (atom nil))
+
+(defn add-radio! [name url]
+  (when (and (not-empty name) (not-empty url))
+    (swap! radios conj {:name name :url url})
+    true))
+
--- src/webradio/ui.clj
+++ src/webradio/ui.clj
@@ -2,16 +2,26 @@
   (:require [webradio.model :as model]
             [webradio.player :as player]
             [webradio.theme :as theme])
-  (:import [javax.swing JFrame JList JScrollPane JPanel]
+  (:import [javax.swing JFrame JList JScrollPane JPanel JTextField JLabel JOptionPane DefaultListModel]
            [javax.swing.event ListSelectionListener]
-           [java.awt BorderLayout FlowLayout]
+           [java.awt BorderLayout FlowLayout GridLayout]
            [java.awt.event ActionListener]))
+
+(defn- update-radio-list! [jlist radios]
+  (let [model (DefaultListModel.)]
+    (doseq [radio radios]
+      (.addElement model (:name radio)))
+    (.setModel jlist model)))
 
 (defn create-ui []
   (theme/apply-dark-theme)
 
   (let [frame (JFrame. "WebRadio Player")
-        radio-list (JList. (into-array String (map :name @model/radios)))
+        add-panel (JPanel. (GridLayout. 2 2 5 5))
+        name-field (JTextField.)
+        url-field (JTextField.)
+        add-button (theme/styled-button "+ Ajouter" theme/dark-theme-colors)
+        radio-list (JList.)
         scroll-pane (JScrollPane. radio-list)
         digital-font (theme/load-digital-font 24)
         status-label (theme/styled-label "--" digital-font theme/dark-theme-colors)
@@ -19,45 +29,81 @@
         prev-button (theme/styled-button "⏮" theme/dark-theme-colors)
         next-button (theme/styled-button "⏭" theme/dark-theme-colors)
         stop-button (theme/styled-button "⏹" theme/dark-theme-colors)
-        control-panel (JPanel.)]
+        control-panel (JPanel.)
+        form-panel (JPanel. (BorderLayout.))
+        main-panel (JPanel. (BorderLayout.))]
 
-    ;; Gestion des événements
-    (.addListSelectionListener radio-list
-                               (reify ListSelectionListener
-                                 (valueChanged [_ e]
-                                   (when-not (.getValueIsAdjusting e)
-                                     (let [index (.getSelectedIndex radio-list)]
-                                       (when (>= index 0)
-                                         (player/play-radio (nth @model/radios index))
-                                         (.setText status-label (.getSelectedValue radio-list))))))))
+    ;; Configuration du formulaire
+    (doto add-panel
+      (.setBackground (:background theme/dark-theme-colors))
+      (.add (JLabel. "Nom:"))
+      (.add name-field)
+      (.add (JLabel. "URL:"))
+      (.add url-field))
 
-    (.addActionListener play-button
-                        (reify ActionListener
-                          (actionPerformed [_ _]
-                            (when-let [index (.getSelectedIndex radio-list)]
-                              (player/play-radio (nth @model/radios index))))))
+    ;; Configuration des boutons
+    (doto form-panel
+      (.setBackground (:background theme/dark-theme-colors))
+      (.add add-panel BorderLayout/CENTER)
+      (.add add-button BorderLayout/EAST))
 
-    (.addActionListener stop-button
-                        (reify ActionListener
-                          (actionPerformed [_ _]
-                            (player/stop-radio)
-                            (.setText status-label "--"))))
+    ;; Initialisation de la liste
+    (update-radio-list! radio-list @model/radios)
 
-    (.addActionListener prev-button
-                        (reify ActionListener
-                          (actionPerformed [_ _]
-                            (let [current (.getSelectedIndex radio-list)]
-                              (when (> current 0)
-                                (.setSelectedIndex radio-list (dec current)))))))
+    ;; CORRECTION: Listener avec la bonne syntaxe
+    (.addListSelectionListener
+     radio-list
+     (reify ListSelectionListener
+       (valueChanged [_ e]  ; Remplacez 'this' par '_' pour indiquer qu'il n'est pas utilisé
+         (when-not (.getValueIsAdjusting e)
+           (let [index (.getSelectedIndex radio-list)]
+             (when (>= index 0)
+               (player/play-radio (nth @model/radios index))
+               (.setText status-label (.getSelectedValue radio-list))))))))
 
-    (.addActionListener next-button
-                        (reify ActionListener
-                          (actionPerformed [_ _]
-                            (let [current (.getSelectedIndex radio-list)
-                                  max-index (dec (count @model/radios))]
-                              (when (< current max-index)
-                                (.setSelectedIndex radio-list (inc current)))))))
+                               (.addActionListener play-button
+                                                   (reify ActionListener
+                                                     (actionPerformed [_ _]
+                                                       (when-let [index (.getSelectedIndex radio-list)]
+                                                         (player/play-radio (nth @model/radios index))))))
 
+                               (.addActionListener stop-button
+                                                   (reify ActionListener
+                                                     (actionPerformed [_ _]
+                                                       (player/stop-radio)
+                                                       (.setText status-label "--"))))
+
+                               (.addActionListener prev-button
+                                                   (reify ActionListener
+                                                     (actionPerformed [_ _]
+                                                       (let [current (.getSelectedIndex radio-list)]
+                                                         (when (> current 0)
+                                                           (.setSelectedIndex radio-list (dec current)))))))
+
+                               (.addActionListener next-button
+                                                   (reify ActionListener
+                                                     (actionPerformed [_ _]
+                                                       (let [current (.getSelectedIndex radio-list)
+                                                             max-index (dec (count @model/radios))]
+                                                         (when (< current max-index)
+                                                           (.setSelectedIndex radio-list (inc current)))))))
+
+                               (.addActionListener add-button
+                                                   (reify ActionListener
+                                                     (actionPerformed [_ _]
+                                                       (let [name (.getText name-field)
+                                                             url (.getText url-field)]
+                                                         (if (model/add-radio! name url)
+                                                           (do
+                                                             (update-radio-list! radio-list @model/radios)
+                                                             (.setText name-field "")
+                                                             (.setText url-field ""))
+                                                           (JOptionPane/showMessageDialog frame
+                                                                                          "Veuillez saisir un nom et une URL valides"
+                                                                                          "Erreur"
+                                                                                          JOptionPane/ERROR_MESSAGE))))))
+
+    ;; Configuration des panneaux
     (doto control-panel
       (.setLayout (FlowLayout.))
       (.setBackground (:background theme/dark-theme-colors))
@@ -66,11 +112,16 @@
       (.add next-button)
       (.add stop-button))
 
+    ;; Assemblage final
+    (doto main-panel
+      (.add form-panel BorderLayout/NORTH)
+      (.add scroll-pane BorderLayout/CENTER)
+      (.add control-panel BorderLayout/SOUTH))
+
     (doto frame
       (.setLayout (BorderLayout.))
-      (.add scroll-pane BorderLayout/CENTER)
-      (.add control-panel BorderLayout/SOUTH)
+      (.add main-panel BorderLayout/CENTER)
       (.add status-label BorderLayout/NORTH)
-      (.setSize 400 500)
+      (.setSize 400 600)
       (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
       (.setVisible true))))
