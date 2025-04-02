f16d545801142d96a939296851d6959b566072c4
jchr <jicelejeune@gmail.com>
2025-04-02 15:21:08 +0200

    station et titre en cours fonctionnels meme si pas parfait

--- src/webradio/metadata.clj
+++ src/webradio/metadata.clj
@@ -1,53 +1,61 @@
 (ns webradio.metadata
   (:import [java.net URL]
-           [java.io BufferedInputStream]))
+           [java.io BufferedInputStream]
+           [java.util.concurrent Executors TimeUnit]))
 
-(defonce metadata-thread (atom nil)) ;; Pour stocker le thread en cours
+;; Atomes pour stocker l'état du fetcher
+(defonce metadata-scheduler (atom nil))
+(defonce last-title (atom nil))
+(defonce error-count (atom 0))
+(def max-errors 5)
+(defonce last-station (atom nil))
 
 (defn extract-title [metadata]
   (when-let [match (re-find #"StreamTitle='([^']+)'" metadata)]
     (second match)))
 
-(defn get-icy-metadata [stream-url]
-  (future
-    (try
-      (let [url (URL. stream-url)
-            conn (.openConnection url)]
-        (.setRequestProperty conn "Icy-MetaData" "1")
-        (.connect conn)
+(defn stop-metadata-fetcher []
+  (when @metadata-scheduler
+    (.shutdown @metadata-scheduler)
+    (reset! metadata-scheduler nil)
+    ;; (println "Metadata fetcher arrêté.")
+    ))
 
-        (let [icy-title (.getHeaderField conn "icy-name")
-              meta-int (.getHeaderFieldInt conn "icy-metaint" -1)]
-          (println "🎙 Station actuelle" icy-title)
-
-          (if (pos? meta-int)
-            (with-open [stream (BufferedInputStream. (.getInputStream conn))]
-              (loop []
-                (let [buffer (byte-array meta-int)]
-                  (.read stream buffer 0 meta-int)
-                  (let [metadata-length (* 16 (.read stream))]
-                    (when (pos? metadata-length)
-                      (let [metadata-buffer (byte-array metadata-length)]
-                        (.read stream metadata-buffer 0 metadata-length)
-                        (let [metadata (String. (.getBytes (String. metadata-buffer "ISO-8859-1") "ISO-8859-1") "UTF-8")
-                              title (extract-title metadata)]
-                          (when title
-                            (println "🎵 Titre en cours: " title))
-                          (Thread/sleep 1000) ;; Plus réactif !
-                          (recur))))))))
-            (println "⚠️ Aucune métadonnée ICY trouvée."))
-
-          {:station icy-title}))
-      (catch Exception e
-        (println "❌ Erreur de récupération des métadonnées: " (.getMessage e))
-        nil))))
-
+(defn fetch-metadata [stream-url]
+  (try
+    (let [url (URL. stream-url)
+          conn (.openConnection url)]
+      (.setRequestProperty conn "Icy-MetaData" "1")
+      (.connect conn)
+      (let [icy-title (.getHeaderField conn "icy-name")
+            meta-int (.getHeaderFieldInt conn "icy-metaint" -1)]
+        ;; N'afficher le nom de la station que s'il a changé
+        (when (and icy-title (not= icy-title @last-station))
+          (reset! last-station icy-title)
+          (println "Station actuelle:" icy-title))
+        (when (pos? meta-int)
+          (with-open [stream (BufferedInputStream. (.getInputStream conn))]
+            (let [buffer (byte-array meta-int)]
+              (.read stream buffer 0 meta-int)
+              (let [metadata-length (* 16 (.read stream))]
+                (when (pos? metadata-length)
+                  (let [metadata-buffer (byte-array metadata-length)]
+                    (.read stream metadata-buffer 0 metadata-length)
+                    (let [metadata (String. metadata-buffer "utf-8")
+                          title (extract-title metadata)]
+                      (when (and title (not= title @last-title))
+                        (reset! last-title title)
+                        (println "Titre en cours:" title))
+                      (reset! error-count 0))))))))))
+    (catch Exception e
+      (swap! error-count inc)
+      (println "Erreur de récupération des métadonnées:" (.getMessage e))
+      (when (>= @error-count max-errors)
+        (println "Trop d'erreurs consécutives, arrêt du fetcher.")
+        (stop-metadata-fetcher)))))
 
 (defn start-metadata-fetcher [stream-url]
-  (when @metadata-thread
-    (println "🔄 Arrêt du thread précédent...")
-    (future-cancel @metadata-thread)) ;; Arrêter le thread précédent proprement
-
-  (reset! metadata-thread
-          (future (get-icy-metadata stream-url)))) ;; Lancer un nouveau thread
-
+  (stop-metadata-fetcher) ;; Arrêter un éventuel fetcher en cours
+  (let [scheduler (Executors/newScheduledThreadPool 1)]
+    (reset! metadata-scheduler scheduler)
+    (.scheduleAtFixedRate scheduler #(fetch-metadata stream-url) 0 10 TimeUnit/SECONDS)))
--- src/webradio/player.clj
+++ src/webradio/player.clj
@@ -4,25 +4,43 @@
   (:import [javazoom.jl.player.advanced AdvancedPlayer]
            [java.net URL]))
 
+(defonce playing? (atom false)) ;; Flag pour signaler si la radio est en cours de lecture
+
 (defn stop-radio []
-  (when-let [player @model/current-player]
-    (.close player)
-    (reset! model/current-player nil)))
+  (when @playing?
+    (reset! playing? false) ;; Désactiver la lecture
+    (when-let [player @model/current-player]
+      (.close player)
+      (reset! model/current-player nil))
+    (metadata/stop-metadata-fetcher))) ;; Arrêter aussi la récupération des métadonnées
 
 (defn play-radio [radio]
-  (stop-radio)
+  (stop-radio) ;; Arrêter l'ancienne radio proprement
   (try
-    (let [url (URL. (:url radio))]
-      ;; Lancer l'extraction des métadonnées en arrière-plan
-      (metadata/get-icy-metadata (:url radio))
-      (let [input-stream (.openStream url)
-            player (AdvancedPlayer. input-stream)]
-        (reset! model/current-player player)
-        (.start (Thread. #(.play player)))))
-    (catch Exception e
-      (println "Erreur de lecture:" (.getMessage e)))))
+    (let [url (URL. (:url radio))
+          input-stream (.openStream url)
+          player (AdvancedPlayer. input-stream)]
+      (reset! playing? true)
+      (reset! model/current-player player)
 
+      ;; Démarrer la récupération des métadonnées
+      (metadata/start-metadata-fetcher (:url radio))
+
+      ;; Démarrer le player dans un thread
+      (.start (Thread.
+               (fn []
+                 (try
+                   (.play player)
+                   (catch Exception e
+                     (println "Erreur de lecture:" (.getMessage e)))
+                   (finally
+                     (reset! playing? false) ;; Réinitialiser le flag après l'arrêt
+                     (reset! model/current-player nil)))))))
+    (catch Exception e
+      (println "Erreur de lecture:" (.getMessage e))
+      (reset! playing? false))))
 
 (comment
   (play-radio {:url "http://direct.franceinter.fr/live/franceinter-midfi.mp3"})
+  (Thread/sleep 5000) ;; Attendre un peu
   (stop-radio))
